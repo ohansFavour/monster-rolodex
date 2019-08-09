d89714d0877d08cd7b219fda155c71b8123a44e6
unknown <fohanekwu@gmail.com>
2019-08-09 15:46:30 +0100

    Updates

---
+++ precache-manifest.6ddb50e0353631129ac9861a797d345f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855bc69e5af5e3d1d6ce4b2bfb9e24ac",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "38e31ab526c3f4fa851f",
+    "url": "/monster-rolodex/static/css/main.2e72a1dc.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monster-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "38e31ab526c3f4fa851f",
+    "url": "/monster-rolodex/static/js/main.90898bf3.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
